e7b3f4ea14856028927975d492ad6a62965c44ff
Joshua Ruff <joshruff@hrg.lab>
2016-11-29 16:58:34 -0600

    Drew Radiation Boundary

--- HFSS_Python/Test2.py
+++ HFSS_Python/Test2.py
@@ -15,13 +15,14 @@ oDesktop.RestoreWindow()
 oDesign=oProject.InsertDesign("HFSS","HFSS_Script_Test", "DrivenModal", "")
 
 
-rectangular_patch_antenna(oDesign,2.45e9, 50, 62, 2.2, "mil", "Patch1")
+[excitation, patch_names]=rectangular_patch_antenna(oDesign,2.45e9, 50, 62, 2.2, "Rogers RT/duroid 5880 (tm)","mil", "Patch1")
 #createRelativeCS(oDesign, 50,10,20,1,2,3,3,2,1,"mm","TestCS")
 #Test drawing functions 
 #rectangular_patch_antenna(oDesign, 2.2e9, 50, 62, 2.2, "mil", "Patch2")
 # assignExcitation(oDesign, "Test Wave")
-
-
+print('patch name list', patch_names)
+drawSphere(oDesign, 0,0,0,100, "mm","vacuum", "radiation_boundary", .55)
+binarySubtraction(oDesign, "radiation_boundary", patch_names,True)
 #oProject.Save()
 
 
